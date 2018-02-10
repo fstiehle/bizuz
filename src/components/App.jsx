8404680511fb04afec0a3b4ea90e19a177b92c87
Fabian Stiehle <f.stiehle@gmail.com>
2018-02-10 16:38:40 +0100

    routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Home from './Home.jsx'
 import Navigation from './Navigation.jsx'
+import Shop from './Shop.jsx'
+import Rent from './Rent.jsx'
+import Detail from './Detail.jsx'
+import Lookbook from './Lookbook.jsx'
+import Info from './Info.jsx'
 import { Route, Switch } from 'react-router-dom'
 
 export default class App extends React.Component {
@@ -9,8 +14,13 @@ export default class App extends React.Component {
   render() {
     return(
       <div>
-        <Navigation />
-        <Switch>          
+        <Route path="/" component={Navigation} />
+        <Switch>
+          <Route path="/shop" exact component={Shop} />
+          <Route path="/rent" exact component={Rent} />
+          <Route path="/lookbook" exact component={Lookbook} />
+          <Route path="/info" exact component={Info} />
+          <Route path="/shop/:id" exact component={Detail} />
           <Route path="/" exact component={Home} />
         </Switch>
       </div>
